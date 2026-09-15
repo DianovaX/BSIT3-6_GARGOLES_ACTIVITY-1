4d482093b76659a0ce6251cc87945dbbefdea4b5
Diana Me Gargoles <difa.gargoles.ui@phinmaed.com>
2026-09-15 09:26:40 +0800

    Switch MySQL package and remove connection logs
    Updated MySQL package from 'mysql2' to 'mysql' and removed connection logging.

--- conn.js
+++ conn.js
@@ -1,18 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'biodata'
-});
-
-conn.connect((err) => {
-    if (err) {
-        console.log('Database connection failed:', err);
-    } else {
-        console.log('Connected to MySQL database!');
-    }
 });
 
 module.exports = conn;
